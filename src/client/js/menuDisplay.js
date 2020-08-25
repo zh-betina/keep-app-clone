12727f938c528ec2d3b72f0408b533c2515f1861
Betina Jasinska <zh.betina@gmail.com>
2020-08-25 11:17:37 +0200

    Add: wide main content on sidebar hidden mode

--- src/client/js/menuDisplay.js
+++ src/client/js/menuDisplay.js
@@ -1,14 +1,19 @@
 function menuDisplay(){
  const sidebar =  document.querySelector('.aside');
  const classList = sidebar.classList;
- console.log(classList);
+ const body = document.querySelector('body');
 
  if(classList.contains('hidden') == false){
    sidebar.classList.add('hidden');
-
+   body.classList.remove('body');
+   body.classList.add('body-wide-main-cont');
  }else{
    sidebar.classList.remove('hidden');
- }
+   body.classList.remove('body-wide-main-cont');
+   body.classList.add('body');
+  }
 }
 
 export { menuDisplay };
+
+// TODO: fix KEEP logo and hambmenu fonts change
